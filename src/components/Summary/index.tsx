f743b34a2443bbf60df51b1dd54d24cff07c2fab
artprange <arthur.prange@gmail.com>
2023-11-12 16:46:54 -0300

    fixing lint

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,45 +1,37 @@
-import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
-import { SummaryCard, SummaryContainer } from "./styles";
-import { useContext } from "react";
-import { TransactionsContext } from "../../contexts/TransactionsContext";
-import { priceFormatter } from "../../utils/formatter";
-import { useSummary } from "../../hooks/useSummary";
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react'
+import { SummaryCard, SummaryContainer } from './styles'
 
+import { priceFormatter } from '../../utils/formatter'
+import { useSummary } from '../../hooks/useSummary'
 
+export function Summary() {
+  const summary = useSummary()
 
-export function Summary(){
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00b37e" />
+        </header>
+        <strong>{priceFormatter.format(summary.income)}</strong>
+      </SummaryCard>
 
-    const summary = useSummary()
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color="#f75a68" />
+        </header>
+        <strong>{priceFormatter.format(summary.expense)}</strong>
+      </SummaryCard>
 
-
-    return (
-        <SummaryContainer>
-            <SummaryCard>
-                <header>
-                    <span>Entradas</span>
-                    <ArrowCircleUp size={32} color="#00b37e"/>
-                    </header>
-                    <strong>{priceFormatter.format( summary.income)}</strong>
-                
-            </SummaryCard>
-
-            <SummaryCard>
-                <header>
-                    <span>Saídas</span>
-                    <ArrowCircleDown size={32} color="#f75a68"/>
-                    </header>
-                    <strong>{priceFormatter.format(summary.expense)}</strong>
-                
-            </SummaryCard>
-
-            <SummaryCard variant= "green">
-                <header>
-                    <span>Total</span>
-                    <CurrencyDollar size={32} color="#FFF"/>
-                    </header>
-                    <strong>{priceFormatter.format(summary.total)}</strong>
-            
-            </SummaryCard>
-        </SummaryContainer>
-    )
+      <SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color="#FFF" />
+        </header>
+        <strong>{priceFormatter.format(summary.total)}</strong>
+      </SummaryCard>
+    </SummaryContainer>
+  )
 }
--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,55 +1,48 @@
-import { useContext, useEffect, useState } from "react";
-import { Header } from "../../components/Header";
-import { Summary } from "../../components/Summary";
-import { SearchForm } from "./components/SearchForm";
-import { PriceHighLight, TransactionsContainer, TransactionsTable } from "./styles";
-import { TransactionsContext } from "../../contexts/TransactionsContext";
-import { dateFormatter, priceFormatter } from "../../utils/formatter";
+import { useContext } from 'react'
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import { SearchForm } from './components/SearchForm'
+import {
+  PriceHighLight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
 
+export function Transactions() {
+  const { transactions } = useContext(TransactionsContext)
 
+  return (
+    <div>
+      <Header />
+      <Summary />
 
+      <TransactionsContainer>
+        <SearchForm />
 
-
-
-
-
-export function Transactions(){
-	const { transactions } = useContext(TransactionsContext);
-
-	return (
-		<div>
-		<Header />
-		<Summary />
-
-		<TransactionsContainer>
-			<SearchForm />
-
-		<TransactionsTable>
-		<tbody>
-			{transactions.map((transaction) => {
-		return (
-			<tr key={transaction.id}>
-			<td width="50%">{transaction.description}</td>
-			<td>
-			<PriceHighLight variant={transaction.type}>
-				{transaction.type === "expense" && "- "}
-				{priceFormatter.format(transaction.price)}
-			</PriceHighLight>
-			</td>
-			<td>
-                {transaction.category}</td>
-			<td>
-				{dateFormatter.format(new Date(transaction.createdAt))}
-			</td>
-			</tr>
-			);
-        }
-    )
-}
-
-		</tbody>
-		</TransactionsTable>
-		</TransactionsContainer>
-		</div>
-	);
+        <TransactionsTable>
+          <tbody>
+            {transactions.map((transaction) => {
+              return (
+                <tr key={transaction.id}>
+                  <td width="50%">{transaction.description}</td>
+                  <td>
+                    <PriceHighLight variant={transaction.type}>
+                      {transaction.type === 'expense' && '- '}
+                      {priceFormatter.format(transaction.price)}
+                    </PriceHighLight>
+                  </td>
+                  <td>{transaction.category}</td>
+                  <td>
+                    {dateFormatter.format(new Date(transaction.createdAt))}
+                  </td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  )
 }
